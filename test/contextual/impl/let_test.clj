199f3ca6b9ec086d018ed0c56eee215e36c47725
ben.sless <ben.sless@appsflyer.com>
2020-11-06 11:39:52 +0200

    Add let tests

--- test/contextual/impl/let_test.clj
+++ test/contextual/impl/let_test.clj
@@ -40,5 +40,50 @@
               {:x 1 :y 2})))))
 
 (t/deftest let-bindings
-  (t/testing "Loop bindings")
-  (t/testing "Unrolled bindings"))
+  (t/testing "Simple bindings"
+    (t/is
+     (=
+      3
+      (p/-invoke (sut/->let
+                  '[a 1 b 2]
+                  (->fn +
+                        (sut/->lookup 'a)
+                        (sut/->lookup 'b))) {}))))
+  (t/testing "Expression bindings"
+    (t/is
+     (=
+      3
+      (p/-invoke (sut/->let
+                  ['a (->path :x)
+                   'b (->path :y)]
+                  (->fn +
+                        (sut/->lookup 'a)
+                        (sut/->lookup 'b)))
+                 {:x 1 :y 2}))))
+  (t/testing "Nested let"
+    (t/is
+     (=
+      3
+      (p/-invoke
+       (sut/->let
+        ['a (->path :x)]
+        (sut/->let
+         ['b (->path :y)]
+         (->fn +
+               (sut/->lookup 'a)
+               (sut/->lookup 'b))))
+       {:x 1 :y 2}))))
+  (t/testing "Shadowing"
+    (t/is
+     (=
+      12
+      (p/-invoke
+       (sut/->let
+        ['a (->path :x)]
+        (sut/->let
+         ['a 10
+          'b (->path :y)]
+         (->fn +
+               (sut/->lookup 'a)
+               (sut/->lookup 'b))))
+       {:x 1 :y 2})))))
