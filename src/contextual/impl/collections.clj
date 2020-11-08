1f6068361ff3ac4bafd55c12407d97e95987caed
ben.sless <ben.sless@appsflyer.com>
2020-11-08 18:25:28 +0200

    Add unrolled map

--- src/contextual/impl/collections.clj
+++ src/contextual/impl/collections.clj
@@ -12,7 +12,46 @@
       (transient {})
       m))))
 
-(defn ->map [m] (->MapWrapper m))
+
+(defonce ^:private map-wrapper-builders (atom {}))
+
+(defmacro ^:private def-map-wrappers []
+  (let [invoke '-invoke
+        ctx 'ctx
+        name "MapWrapper"
+        defs
+        (for [n (range 1 13)
+              :let [ks (map (comp symbol #(str "k" %)) (range n))
+                    vs (map (comp symbol #(str "v" %)) (range n))
+                    rec (symbol (str name n))
+                    constructor (symbol (str "->" rec))
+                    body (reduce-kv
+                          (fn [m k v]
+                            (assoc m `(p/-invoke ~k ~ctx) `(p/-invoke ~v ~ctx)))
+                          {}
+                          (zipmap ks vs))]]
+          `(do
+             (defrecord ~rec [~@(interleave ks vs)]
+               p/IContext
+               (~invoke [~'this ~ctx]
+                ~body))
+             (swap! map-wrapper-builders assoc ~n ~constructor)))]
+    `(do
+       ~@defs)))
+
+(def-map-wrappers)
+
+(defn ->map
+  [m]
+  (let [n (count m)
+        args (mapcat identity m)
+        c (get @map-wrapper-builders n)]
+    (if c
+      (apply c args)
+      (->MapWrapper args))))
+
+(comment
+  (->map {:a 1 :b 2}))
 
 (defrecord VectorWrapper [v]
   p/IContext
