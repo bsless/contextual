e7a73d1b2632fc1402cd3d714690bda156bc451f
ben.sless <ben.sless@appsflyer.com>
2020-12-14 13:31:11 +0200

    Add deploy repositories and bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
-(defproject bsless/contextual "0.0.0-alpha1-SNAPSHOT"
+(defproject bsless/contextual "0.0.0-alpha1"
   :description "Deferred evaluation with context"
   :url "https://github.com/bsless/contextual"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_token
+                                    :sign-releases false}]
+                        ["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev {:source-paths ["./dev"]
                    :dependencies [[org.clojure/tools.trace "0.7.10"]
